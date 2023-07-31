6d1a12c1c553f145b550b08ca5f244d3ef793490
JazzRDS <jazzrds@gmail.com>
2023-07-31 08:27:56 +0100

    lab done except navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,14 @@
-function Navbar() {}
+// src/components/Navbar.jsx
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <div className="container">
+        <span className="navbar-brand">WikiCountries</span>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
